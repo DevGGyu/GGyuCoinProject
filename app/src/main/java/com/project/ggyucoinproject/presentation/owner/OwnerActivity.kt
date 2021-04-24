5dc535b2327493fe5385c65deaa8c5a662ab43eb
dev.ggyu <dev.ggyu@gmail.com>
2021-04-24 13:30:10 +0900

    검색바 추가
    Signed-off-by: dev.ggyu <dev.ggyu@gmail.com>

--- app/src/main/java/com/project/ggyucoinproject/presentation/owner/OwnerActivity.kt
+++ app/src/main/java/com/project/ggyucoinproject/presentation/owner/OwnerActivity.kt
@@ -1,6 +1,8 @@
 package com.project.ggyucoinproject.presentation.owner
 
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.project.ggyucoinproject.R
@@ -14,5 +16,25 @@ class OwnerActivity : AppCompatActivity() {
             .setContentView<ActivityOwnerBinding>(this, R.layout.activity_owner)
 
         setSupportActionBar(binding.toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        if (menu == null) return false
+
+        menuInflater.inflate(R.menu.menu, menu)
+        val sv = menu.findItem(R.id.coin_search).actionView as SearchView
+        sv.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener,
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+        return true
     }
 }
