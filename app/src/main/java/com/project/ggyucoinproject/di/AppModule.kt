aa489c99d4acccf8f24c50c8ce4e57a5b28cc64b
dev.ggyu <dev.ggyu@gmail.com>
2021-05-24 22:32:25 +0900

    Module 통합관리
    Signed-off-by: dev.ggyu <dev.ggyu@gmail.com>

--- app/src/main/java/com/project/ggyucoinproject/di/AppModule.kt
+++ app/src/main/java/com/project/ggyucoinproject/di/AppModule.kt
@@ -1,12 +1,16 @@
-package com.project.ggyucoinproject.etc.module
+package com.project.ggyucoinproject.di
 
+import android.content.Context
+import androidx.room.Room
 import com.project.ggyucoinproject.BuildConfig
 import com.project.ggyucoinproject.etc.api.MarketService
+import com.project.ggyucoinproject.etc.db.MainDatabase
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -14,9 +18,22 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
 @Module
-class NetworkModule {
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): MainDatabase {
+        return Room
+            .databaseBuilder(context, MainDatabase::class.java, "market-db")
+            .build()
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
 
     @Singleton
     @Provides
